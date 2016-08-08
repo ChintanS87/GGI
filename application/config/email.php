e31be5efe9b558f2eab1e420c492aad4bc35c26e
rajuldm <rajuldm@gmail.com>
2016-08-08 17:15:40 +0530

    facebook login

--- application/config/email.php
+++ application/config/email.php
@@ -10,6 +10,7 @@
 |	http://codeigniter.com/user_guide/libraries/email.html
 |
 */
+/*
 $config['protocol'] = 'smtp';
 $config['smtp_host'] = 'ssl://smtp.googlemail.com';
 $config['smtp_user'] = 'hajiali@shrimadrajchandramission.org';
@@ -19,6 +20,16 @@ $config['charset']    = 'utf-8';
 $config['newline']    = "\r\n";
 $config['mailtype'] = 'html'; // or html
 $config['validation'] = false; // bool whether to validate email or not
+*/
 
+$config['protocol'] = 'smtp';
+$config['smtp_host'] = 'relay-hosting.secureserver.net';
+$config['smtp_user'] = 'hajiali@shrimadrajchandramission.org';
+$config['smtp_pass'] = 'GURUPREM';
+$config['smtp_port'] = '465';
+$config['charset']    = 'utf-8';
+$config['newline']    = "\r\n";
+$config['mailtype'] = 'html'; // or html
+$config['validation'] = false; // bool whether to validate email or not
 /* End of file email.php */
 /* Location: ./application/config/email.php */
