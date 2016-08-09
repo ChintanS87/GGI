cf07fdcda879b3c2a1725027fc55f00ba7cf3f6d
rajuldm <rajuldm@gmail.com>
2016-08-09 15:22:24 +0530

    facebooklogin

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -6,15 +6,15 @@ class Test extends CI_Controller
 public function sendemail(){
         $config = Array(
         'protocol' => 'smtp',
-        'smtp_host' => 'ssl://smtp.googlemail.com',
-        'smtp_port' => 465,
+        'smtp_host' => 'relay-hosting.secureserver.net',
+        'smtp_port' => '25',
         'smtp_user' => 'hajiali@shrimadrajchandramission.org',
         'smtp_pass' => 'GURUPREM',
         );
         $this->load->library('email', $config);
         $this->email->set_newline("\r\n");
-        $this->email->from('hajiali@shrimadrajchandramission.org', 'Deepak');
-        $this->email->to('deepakdm8@gmail.com');
+        $this->email->from('hajiali@shrimadrajchandramission.org', 'GoGrabIt');
+        $this->email->to('rajuldm@gmail.com');
         $this->email->subject('Test Mail');
         $this->email->message('Test Codeigniter mail');
         if (!$this->email->send()) {
