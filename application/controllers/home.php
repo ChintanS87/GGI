c18511503e98c15258160c675fea97eecab42a81
rajuldm <rajuldm@gmail.com>
2016-08-10 17:55:51 +0530

    start other gograbit changes

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -10,21 +10,62 @@ class Home extends __APP__
 		$this->load->helper('url');
 		$this->load->library('tank_auth');
 	}
-*/
+    */
+    
+    public function index()
+    {
+        $live_products_array = array();           
+        $this->data['live_auctions'] = $this->auction->where('is_active','Y')->get();
+        foreach($this->data['live_auctions'] as $row) {        
+            array_push($live_products_array, $row->product_id) ;
+        }        
+        $this->data['live_products'] = $this->product->where_in('product_id',$live_products_array)->get();
+        
+
+
+        $auction2 = new Auction();
+        $product2 = new Product();
+        $upcoming_products_array = array();           
+        $this->data['upcoming_auctions'] = $auction2->where('is_active','U')->get();
+        foreach($this->data['upcoming_auctions'] as $row) {        
+            array_push($upcoming_products_array, $row->product_id) ;
+        }        
+        $this->data['upcoming_products'] = $product2->where_in('product_id',$upcoming_products_array)->get();
+
+        
+        $auction3 = new Auction();
+        $product3 = new Product();        
+        $closed_products_array = array();           
+        $this->data['closed_auctions'] = $auction3->where('is_active','C')->get();
+        foreach($this->data['closed_auctions'] as $row) {        
+            array_push($closed_products_array, $row->product_id) ;
+        }        
+        $this->data['closed_products'] = $product3->where_in('product_id',$closed_products_array)->get();
+
+        
+        
+        
+        $this->load->view('home', $this->data);
+    }
+    
+    
+    /*
 	public function index()
 	{
-            $this->data = array();
-		if (!$this->tank_auth->is_logged_in()) {
-			redirect('/auth/login/');
-		} else {
-			$this->data['user_id']	= $this->tank_auth->get_user_id();
-			$this->data['username']	= $this->tank_auth->get_username();
-                        
-                        
-                        //$this->data['search_results'] = $this->auction->get();
-			$this->load->view('home', $this->data);
-		}
+            if (!$this->tank_auth->is_logged_in()) {
+                    redirect('/auth/login/');
+            } else {
+                    $this->data['user_id']	= $this->tank_auth->get_user_id();
+                    $this->data['username']	= $this->tank_auth->get_username();
+
+
+                    //$this->data['search_results'] = $this->auction->get();
+                    $this->load->view('home', $this->data);
+            }
 	}
+        
+      */  
+        
 }
 
 /* End of file home.php */
--- application/views/home.php
+++ application/views/home.php
@@ -1,12 +1,42 @@
-Hi, <strong><?php echo $username; ?></strong>! You are logged in now. <?php echo anchor('/auth/logout/', 'Logout'); ?>
+
+Hi, <strong>
+    <?php //echo $username; ?>
+</strong>! You are logged in now. 
+    <?php //echo anchor('/auth/logout/', 'Logout'); ?>
 <br/><br/>
+
+
+<?php echo anchor('/auth/login/', 'Login'); ?>
+<br/><br/>
+<?php echo anchor('/auth/register/', 'Register Now'); ?>
+<br/><br/>
+<?php echo anchor('', 'Refer a friend'); ?>
+<br/><br/>
+
+
+Live Products<br/>
 <?php
-/*
- $ctr=0; foreach ($search_results as $row) { $ctr++; 
+ $ctr=0; foreach ($live_products as $row) { $ctr++; 
 echo $row->prod_name;
 echo "<br/>";
  }
- 
- */ 
+ ?>
 
+
+<br/><br/> 
+Upcoming Products<br/>
+<?php
+ $ctr=0; foreach ($upcoming_products as $row) { $ctr++; 
+echo $row->prod_name;
+echo "<br/>";
+ }
+?> 
+
+<br/><br/>
+Closed Products<br/>
+<?php
+ $ctr=0; foreach ($closed_products as $row) { $ctr++; 
+echo $row->prod_name;
+echo "<br/>";
+ } 
 ?>
